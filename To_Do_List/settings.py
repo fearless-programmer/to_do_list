8cdd3e5abf6a3615827cdd7de5d1277cef458bd7
Trevor McDaniel <106666268+fearless-programmer@users.noreply.github.com>
2023-07-26 12:27:31 +0300

    Update settings.py

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'theplanhub9@gmail.com'
+EMAIL_HOST_USER = None
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'npdjfpxnaryawgyq'
+EMAIL_HOST_PASSWORD = None
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
